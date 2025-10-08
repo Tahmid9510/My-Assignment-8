3f892f2287e0a0e8056ee95076b63490eae36e1e
Tahmid Hasan <tmd999510@gmail.com>
2025-10-08 16:15:44 +0600

    Banner Section updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     return (
         <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
+            <div className="ml-5 navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -48,7 +48,7 @@ const Navbar = () => {
                             }`}><Link to='/InstalledApps'>Installation</Link></li>
                 </ul>
             </div>
-            <div className="navbar-end">
+            <div className="navbar-end mr-5">
                 <a href='https://github.com/Tahmid9510' className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white"><FaGithub />Contribute</a>
             </div>
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import GooglePlayImg from '../assets/Gplay.png'
+import AppsStoreImg from '../assets/Astore.png'
+import HeroImg from '../assets/hero.png'
 
 const Home = () => {
     return (
         <div>
-            <h1>HOME</h1>
+            <section className='Banner my-10'>
+                <div className='text-center'>
+                    <p className='font-bold text-5xl my-1.5'>We Build</p>
+                    <p className='font-bold text-5xl mb-3'><span className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent'>Productive</span> Apps</p>
+                    <p className='text-gray-500'>At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+                </div>
+                <div className='flex justify-center items-center gap-2.5 my-5'>
+                    <a href="https://play.google.com/store/games?hl=en&pli=1"><button className='btn'> <img src={GooglePlayImg} alt="" />Google Play</button></a>
+                    <a href="https://www.apple.com/app-store/"><button className='btn'><img src={AppsStoreImg} alt="" />App Store</button></a>
+                </div>
+                <div className='flex justify-center mt-7 mb-0'>
+                    <img className='w-[690px]' src={HeroImg} alt="" />
+                </div>
+                <div className='py-14 bg-gradient-to-r from-[#632EE3] to-[#9F62F2] border mt-0 text-white'>
+                    <div className='text-center font-semibold text-4xl'><p>Trusted by Millions, Built for You</p></div>
+                    <div className='flex justify-around'>
+                        <div className='space-y-3 flex flex-col justify-center items-center'>
+                            <p className='text-gray-200 text-[0.7rem]'>Total Downloads</p>
+                            <p className='font-extrabold text-6xl'>29.6M</p>
+                            <p className='text-gray-200 text-[0.7rem]'>21% more than last month</p>
+                        </div>
+                        <div className='space-y-3 flex flex-col justify-center items-center'>
+                            <p className='text-gray-200 text-[0.7rem]'>Total Reviews</p>
+                            <p className='font-extrabold text-6xl'>906K</p>
+                            <p className='text-gray-200 text-[0.7rem]'>46% more than last month</p>
+                        </div>
+                        <div className='space-y-3 flex flex-col justify-center items-center'>
+                            <p className='text-gray-200 text-[0.7rem]'>Active Apps</p>
+                            <p className='font-extrabold text-6xl'>132+</p>
+                            <p className='text-gray-200 text-[0.7rem]'>31 more will Launch</p>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+
+            <section className='Trending'>
+                <div className='text-center space-y-4'>
+                    <p className='font-bold text-5xl'>Trending Apps</p>
+                    <p className='text-gray-500'>Explore All Trending Apps on the Market developed by us</p>
+                </div>
+                <div className='Trending-card'>
+
+                </div>
+            </section>
+
         </div>
     );
 };
